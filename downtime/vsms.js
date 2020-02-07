742129fb29e5520a6076c100ac80f94b3229e409
Frederico Thofehrn <42624209+Fredmty@users.noreply.github.com>
2020-02-07 14:40:30 -0300

    BubbleSort!

--- downtime/vsms.js
+++ downtime/vsms.js
@@ -174,4 +174,18 @@ function strSearch(str,sub){
         }
         return match;
     }
+function bubbleSort (arr) {
 
+    let len = arr.length;
+    let swapped;
+for( let i = 0; i < len; i++) {
+    if (arr[i] > arr[i + 1]) {
+    let temp = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i+1] = temp;
+    swapped = true;
+    }
+}
+while (swapped);
+return true;
+};
